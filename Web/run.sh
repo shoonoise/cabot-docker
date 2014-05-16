137514f53a8f0f7b77dcec9cc63ebcdbd58651fa
Alexander Kushnarev <avkushnarev@gmail.com>
2014-05-16 14:39:44 +0400

    Add email alerting Change source repo env var export refactoring

--- Web/run.sh
+++ Web/run.sh
@@ -6,5 +6,5 @@ python manage.py compress --force &&\
 python manage.py syncdb --noinput && \
 python manage.py migrate && \
 python manage.py loaddata fixture.json &&\
-gunicorn wsgi:application --config gunicorn.conf &&\
+gunicorn wsgi:application --config gunicorn.conf &\
 celery worker -B -A app.cabotapp.tasks --loglevel=INFO --concurrency=16 -Ofair
