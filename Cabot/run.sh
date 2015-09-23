610ab183ae98132da910ae44afcfe9d67450afbc
shoonoise <avkushnarev@gmail.com>
2015-09-23 16:34:19 +0300

    Remove `migrate.sh` as unusable

--- Cabot/run.sh
+++ Cabot/run.sh
@@ -2,10 +2,16 @@
 
 export DATABASE_URL="postgres://docker:docker@db:5432/docker"
 export CELERY_BROKER_URL="redis://celery_broker:6379/1"
-export SES_HOST="$MAIL_EXIM_HOST"
+export SES_HOST="smtp_server"
 export SES_USER=""
 export SES_PASS=""
-export SES_PORT="225"
+export SES_PORT="25"
+
+python manage.py collectstatic --noinput &&\
+python manage.py compress --force &&\
+python manage.py syncdb --noinput && \
+python manage.py migrate && \
+python manage.py loaddata fixture.json
 
 gunicorn cabot.wsgi:application --config gunicorn.conf --log-level info --log-file /var/log/gunicorn &\
 celery worker -B -A cabot --loglevel=INFO --concurrency=16 -Ofair
