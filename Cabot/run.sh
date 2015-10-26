9fa14c7a4a58eb743796c5e9f8e8da29d9d06857
BirgerK <birger.kamp@iteratec.de>
2015-10-26 18:07:16 +0100

    moved email-settlings to env-file for more configuration

--- Cabot/run.sh
+++ Cabot/run.sh
@@ -2,10 +2,6 @@
 
 export DATABASE_URL="postgres://docker:docker@db:5432/docker"
 export CELERY_BROKER_URL="redis://celery_broker:6379/1"
-export SES_HOST="smtp_server"
-export SES_USER=""
-export SES_PASS=""
-export SES_PORT="25"
 
 python manage.py collectstatic --noinput &&\
 python manage.py compress --force &&\
